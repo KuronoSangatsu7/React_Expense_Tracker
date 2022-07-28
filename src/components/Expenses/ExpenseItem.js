57dce91853c751fe90eec6a513ff6f854b6e5f12
Jaffar <jacepac264@gmail.com>
2022-07-28 16:43:34 +0300

    adapted app for mobile devices

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
   return (
-    <div className="container grid grid-cols-12 pl-2 border-0 border-l-4 border-violet-400 my-5">
+    <div className="container md:grid md:grid-cols-12 flex flex-wrap pl-2 border-0 border-l-4 border-violet-400 my-5">
     <div className="col-span-1">
       <ExpenseDate date={props.date} />
       </div>
