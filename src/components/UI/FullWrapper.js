928c9816272709da58e85776861581e80bfd8469
Jaffar <jacepac264@gmail.com>
2022-07-29 17:21:00 +0300

    added background

--- src/components/UI/FullWrapper.js
+++ src/components/UI/FullWrapper.js
@@ -1,9 +1,19 @@
-
+import BackgroundImage from "../../bg.jpg";
 
 const FullWrapper = (props) => {
-    return <div className="h-full overflow-scroll bg-gradient-to-tr from-pink-800 to-slate-900">
-        {props.children}
+  return (
+    <div
+      className="h-full overflow-scroll bg-gradient-to-tr from-pink-800 to-slate-900"
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundClip: "content-box",
+        backgroundPosition: "center",
+        backgroundSize: "1920px 1080px",
+      }}
+    >
+      {props.children}
     </div>
-}
+  );
+};
 
 export default FullWrapper;
