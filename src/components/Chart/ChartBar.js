4ec194f1df84109571c4184b0dc24102d4a3e356
Jaffar <jacepac264@gmail.com>
2022-07-29 14:19:56 +0300

    month names in chart are now fully visible

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -6,7 +6,7 @@ const ChartBar = (props) => {
       <div className="mx-1 h-44 bg-white border-black rounded-full flex">
         <div className="bg-violet-400 w-full rounded-full mt-auto transition-all duration-500" style={{height: barFullHeight}}></div>
       </div>
-      <div className="m-1 text-xs overflow-hidden">{props.month}</div>
+      <div className="m-1 text-xs overflow-visible">{props.month}</div>
     </div>
   );
 };
